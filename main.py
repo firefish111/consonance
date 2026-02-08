609d5cbeb041659d82afcaebdc383bc2eb63212c
firefish111 <firefish111@protonmail.com>
2026-02-08 21:52:52 +0000

    Added interpolation of diphthongs

--- main.py
+++ main.py
@@ -11,17 +11,19 @@ import synth
 #sentence = "the quick brown fox jumps over the lazy dog"
 #sentence = "The beige hue on the waters of the loch impressed all, including the French queen, before she heard that2 symphony again, just2 as2 young Arthur wanted."
 #sentence = "trap strut lot the dress kit foot start nurse fleece north goose price face mouth goat square near choice cure"
-sentence = "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible. Yellow, black. Yellow, black. Yellow, black. Yellow, black."
+sentence = "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible. Yellow, black. Yellow, black. Yellow, black. Yellow, black. Black and yellow. Black and yellow. Black and yellow."
+
+TIME_PER_SYLLABLE = .3
+VOLUME = .1
 
 tones = []
 for word in sentence.split(" "):
   w = Word(word.strip('.,'))
   print(w.vowels())
-  formants = itertools.chain.from_iterable(
-    phones_list.vowels[phone] for phone in w.vowels()
-  )
-
-  for fm in formants:
-    tones.append(synth.synth(.2, .1, *fm))
+  for fm in (phones_list.vowels[phone] for phone in w.vowels()):
+    if len(fm) >= 2:
+      tones.append(synth.synth(TIME_PER_SYLLABLE, VOLUME, *fm[0], interpolate_with=fm[1]))
+    else:
+      tones.append(synth.synth(TIME_PER_SYLLABLE, VOLUME, *fm[0]))
 
 synth.write("sentence.wav", *tones)
--- synth.py
+++ synth.py
@@ -4,13 +4,31 @@ import itertools
 
 SAMPLE_RATE = 8000 # in Hz
 
+def normalise(freqs, t, total):
+  n = freqs[0] + (freqs[1] - freqs[0])*(t/total)
+  n *= 2 * math.pi
+  if freqs[1] > freqs[0]:
+    n *= t
+  else:
+    n *= (total - t)
+  return n
+
 # generator
-def synth(secs, amplitude, *args):
+def synth(secs, amplitude, *args, **kwargs):
+  print(args, len(args), kwargs)
+  if "interpolate_with" in kwargs:
+    mid = len(args) // 2
+    data = list(zip(args, kwargs["interpolate_with"]))
+    print(data, kwargs)
+
   for sample in range(round(secs * SAMPLE_RATE)): # tick for that many samples
     # theta: progresses by 2pi every second
     t = sample / SAMPLE_RATE
-    theta = t * 8 * math.atan(1) # *2pi
-    wave = sum(math.sin(theta * freq) for freq in args) * amplitude
+    theta = t * 2 * math.pi
+    if "interpolate_with" in kwargs:
+      wave = sum(math.sin(normalise(freqs, t, secs)) for freqs in data) * amplitude
+    else:
+      wave = sum(math.sin(theta * freq) for freq in args) * amplitude
     clamped = max(-1, min(wave, 1))
 
     yield math.floor((clamped + 1) * 127.5) # map it onto 0-255
