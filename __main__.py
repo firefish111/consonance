900900c7aef110f8adb8e4a0a27cf130841a966d
firefish <firefish111@protonmail.com>
2026-02-19 23:50:21 +0000

    Create flask boilerplate, and also uv project

--- __main__.py
+++ __main__.py
@@ -1,6 +1,14 @@
 #! /usr/bin/python
 
+import flask
 import sounds
 
-# stub.
-# TODO: use with flask
+# __name__ is the package name.
+# we use this so that we can hunt for templates, static files, etc
+app = flask.Flask(__name__)
+
+@app.route("/")
+def root():
+  return flask.render_template("index.html")
+
+app.run()
